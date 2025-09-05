fc058546b0405848480003b75011cc1a7c58bba8
Prakash K <prakash@HQ.CLI.IN>
2025-09-05 16:03:51 +0530

    Added Project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import MatrimonyProfileBuilder from "./MatrimonyProfileBuilder";
+
+export default function App() {
+  return (
+    <>
+      <MatrimonyProfileBuilder />
+    </>
+  );
+}
