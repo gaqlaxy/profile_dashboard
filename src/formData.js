f3c83dccefcb1b29f2e80f99babfb7b8bd5d3e43
gaqlaxy <v4vripper@gmail.com>
2025-09-08 06:57:20 +0530

    fixed raasi box sizes

--- src/formData.js
+++ src/formData.js
@@ -166,38 +166,69 @@ export const fieldPositions = {
   address: { top: 210, left: 35 },
 
   // Photo
-  photo: { top: 55, right: 0, width: 102, height: 147 },
+  photo: { top: 55, right: -0.4, width: 102, height: 147 },
 
   // Horoscope (wrapper boxes + individual cells)
   raasi: { top: 211, left: 2, size: 70 },
+  // raasiCells: [
+  //   { top: 0, left: 18, w: 19, h: 16 }, // 1
+  //   { top: 20, left: 60, w: 18, h: 16 }, // 3
+  //   { top: 40, left: 60, w: 18, h: 16 }, // 4
+  //   { top: 60, left: 60, w: 18, h: 16 }, // 5
+  //   { top: 60, left: 40, w: 18, h: 16 }, // 6
+  //   { top: 60, left: 20, w: 18, h: 16 }, // 7
+  //   { top: 40, left: 0, w: 18, h: 16 }, // 8
+  //   { top: 20, left: 0, w: 18, h: 16 }, // 9
+  //   { top: 0, left: 0, w: 18, h: 16 }, // 10
+  //   { top: 0, left: 60, w: 18, h: 16 }, // 11
+  //   { top: 20, left: 40, w: 18, h: 16 }, // 12
+  //   { top: 0, left: 37, w: 19, h: 16 }, // 2
+  // ],
   raasiCells: [
-    { top: 0, left: 18, w: 19, h: 16 }, // 1
-    { top: 20, left: 60, w: 18, h: 16 }, // 3
-    { top: 40, left: 60, w: 18, h: 16 }, // 4
-    { top: 60, left: 60, w: 18, h: 16 }, // 5
-    { top: 60, left: 40, w: 18, h: 16 }, // 6
-    { top: 60, left: 20, w: 18, h: 16 }, // 7
-    { top: 40, left: 0, w: 18, h: 16 }, // 8
-    { top: 20, left: 0, w: 18, h: 16 }, // 9
-    { top: 0, left: 0, w: 18, h: 16 }, // 10
-    { top: 0, left: 60, w: 18, h: 16 }, // 11
-    { top: 20, left: 40, w: 18, h: 16 }, // 12
-    { top: 0, left: 37, w: 19, h: 16 }, // 2
+    { top: 0, left: 37, w: 19, h: 16 }, // 1 (top middle)
+    { top: 0, left: 56.3, w: 18, h: 16 }, // 2 (top right)
+    { top: 16, left: 56.3, w: 18, h: 17 }, // 3
+    { top: 33, left: 56.3, w: 18, h: 16 }, // 4
+    { top: 49, left: 56.3, w: 18, h: 15.5 }, // 5 (bottom right)
+    { top: 49, left: 37.6, w: 18, h: 15.5 }, // 6
+    { top: 49, left: 18.3, w: 18.5, h: 15.5 }, // 7 (bottom middle)
+    { top: 49, left: 0, w: 18, h: 15.5 },  // 8
+    { top: 33, left: 0, w: 18, h: 16 },  // 9
+    { top: 16, left: 0, w: 18, h: 17 },  // 10
+    { top: 0, left: 0, w: 18, h: 16 },   // 11 (top left)
+    { top: 0, left: 18, w: 19, h: 16 },  // 12
   ],
 
+
   navamsa: { top: 250, right: 30, size: 70 },
+  // navamsaCells: [
+  //   { top: 0, left: 20, w: 18, h: 16 },
+  //   { top: 0, left: 40, w: 18, h: 16 },
+  //   { top: 20, left: 60, w: 18, h: 16 },
+  //   { top: 40, left: 60, w: 18, h: 16 },
+  //   { top: 60, left: 60, w: 18, h: 16 },
+  //   { top: 60, left: 40, w: 18, h: 16 },
+  //   { top: 60, left: 20, w: 18, h: 16 },
+  //   { top: 40, left: 0, w: 18, h: 16 },
+  //   { top: 20, left: 0, w: 18, h: 16 },
+  //   { top: 0, left: 0, w: 18, h: 16 },
+  //   { top: 0, left: 60, w: 18, h: 16 },
+  //   { top: 20, left: 40, w: 18, h: 16 },
+  // ],
   navamsaCells: [
-    { top: 0, left: 20, w: 18, h: 16 },
-    { top: 0, left: 40, w: 18, h: 16 },
-    { top: 20, left: 60, w: 18, h: 16 },
-    { top: 40, left: 60, w: 18, h: 16 },
-    { top: 60, left: 60, w: 18, h: 16 },
-    { top: 60, left: 40, w: 18, h: 16 },
-    { top: 60, left: 20, w: 18, h: 16 },
-    { top: 40, left: 0, w: 18, h: 16 },
-    { top: 20, left: 0, w: 18, h: 16 },
-    { top: 0, left: 0, w: 18, h: 16 },
-    { top: 0, left: 60, w: 18, h: 16 },
-    { top: 20, left: 40, w: 18, h: 16 },
+    { top: 0, left: 20, w: 18, h: 16 }, // 1 (top middle)
+    { top: 0, left: 40, w: 18, h: 16 }, // 2 (top right)
+    { top: 20, left: 60, w: 18, h: 16 }, // 3
+    { top: 40, left: 60, w: 18, h: 16 }, // 4
+    { top: 60, left: 60, w: 18, h: 16 }, // 5 (bottom right)
+    { top: 60, left: 40, w: 18, h: 16 }, // 6
+    { top: 60, left: 20, w: 18, h: 16 }, // 7 (bottom middle)
+    { top: 60, left: 0, w: 18, h: 16 },  // 8
+    { top: 40, left: 0, w: 18, h: 16 },  // 9
+    { top: 20, left: 0, w: 18, h: 16 },  // 10
+    { top: 0, left: 0, w: 18, h: 16 },   // 11 (top left)
+    { top: 0, left: 60, w: 18, h: 16 },  // 12
   ],
+
 };
+
